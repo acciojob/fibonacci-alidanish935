f1a3ad99c6f33735146090bdf3ed3ae9af4a1e62
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-08-20 12:37:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-function fibonacci(num) {
+// function fibonacci(num) {
 	
-// your code here
+// // your code here
 
-	if(num == 1)return 0;
-	if(num == 2)return 1;
+// 	if(num == 1)return 0;
+// 	if(num == 2)return 1;
 
-	return fibonacci(num-1)+fibonacci(num-2);
+// 	return fibonacci(num-1)+fibonacci(num-2);
+// }
+
+// module.exports = fibonacci;
+
+
+function fibonacci(num) {
+let a=0
+let b=1
+let c=0
+    if(num==1){
+     return 0
+    }
+    else if(num==2){
+        return 1
+    }
+    else{
+     for(let i=3;i<=num;i++){
+	c=a+b
+	a=b
+	b=c
 }
-
+	return c
+    }
+	
+}
 module.exports = fibonacci;
